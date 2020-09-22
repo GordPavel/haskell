75d295b602ec61e50fbe41d3af7f791c37b5dad2
Гордеев Павел Васильевич <pgordeev@alfabank.ru>
2020-09-22 13:35:48 +0300

    fast recursive function

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,6 @@
 module Lib
   ( fibonacci,
-
+    seqA,
   )
 where
 
@@ -10,3 +10,12 @@ fibonacci = fib 0 1
     fib a b n
       | n <= 1 = b
       | otherwise = fib b (a + b) (n -1)
+
+seqA :: Int -> Int
+seqA = seqA' 3 2 1
+  where
+    seqA' first second third n
+      | n == 2 = first
+      | n == 1 = second
+      | n == 0 = third
+      | otherwise = seqA' (first + second - 2 * third) first second (n -1)
